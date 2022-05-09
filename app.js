e93048f89611c7afe4217397d8e9843539e8ee33
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-09 19:46:41 -0300

    Add 1st security level. Login with username and pasword

--- app.js
+++ app.js
@@ -24,14 +24,6 @@ const User = new mongoose.model('User', userSchema);
 
 
 
-
-
-
-
-
-
-
-
 app.get('/', function(req, res) {
     res.render('home');
 });
@@ -40,10 +32,40 @@ app.get('/login', function(req, res) {
     res.render('login');
 });
 
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, foundname) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundname) {
+                if(foundname.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+
 app.get('/register', function(req, res) {
     res.render('register');
 });
 
+app.post('/register', function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err) {
+        if(!err) {
+            res.render('secrets');
+        } else {
+            console.log(err);
+        }
+    });
+});
 
 
 
