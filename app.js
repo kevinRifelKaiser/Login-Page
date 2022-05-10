7b5c376d0c94daaa533b8837285dca2a17e2ccdb
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-10 20:17:41 -0300

    Change encryption method to hashing method

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.set('view engine', 'ejs');
 const mogoose = require('mongoose');
 const { default: mongoose } = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/userDB');
-//Mongoose-Encryption
-const encrypt = require('mongoose-encryption');
+//MD5 for hashing
+const md5 = require('md5');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,8 +23,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -40,7 +38,7 @@ app.get('/login', function(req, res) {
 
 app.post('/login', function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundname) {
         if(err) {
             console.log(err);
@@ -62,7 +60,7 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if(!err) {
