09c8e76771c09891c9e80a74631f8f846329be91
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-10-03 13:49:50 -0300

    Update enviromental variables from config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 //We can create an .env file in case we do not want to share our keys on github or others spaces when we upload our proyect. But in this case we are going to use de enviromental variables that Railway offer to us.
-export const SECRET= 'thisisoursecretkey';
-export const CLIENT_ID= '948355432444-78cstn3jbu5r395fmqrheojrn9ij2ads.apps.googleusercontent.com';
-export const CLIENT_SECRET= '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';
+export const SECRET = process.env.SECRET;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET;
