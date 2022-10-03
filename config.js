6eda6b60090a11d40ce3c3bcf4e508a8809c0ae4
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-10-03 13:53:15 -0300

    Update port

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 //We can create an .env file in case we do not want to share our keys on github or others spaces when we upload our proyect. But in this case we are going to use de enviromental variables that Railway offer to us.
+export const PORT = process.env.PORT || 3000;
 export const SECRET = process.env.SECRET;
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
